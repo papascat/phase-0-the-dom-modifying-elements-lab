294255e419df5bae1a208ff989d708b7c38fb28f
catherinegay <papascat@aol.com>
2023-03-18 16:29:24 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+let main=document.querySelector('main')
+main.remove()
+let newHeader=document.createElement("h1")
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML="Cat is the champion"
